30be97c91f7c26a48402c52b5760baf7a77b8673
yangfei <644752026@qq.com>
2019-11-25 14:48:56 +0800

    Add new project

---
+++ JavaStudy/src/test/java/com/yf/java/study/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yf.java.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
