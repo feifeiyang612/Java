116049fe417d5b1cd00b9d841445cea68f99550a
yangfei <644752026@qq.com>
2019-11-26 00:03:32 +0800

    add arrayTest

---
+++ JavaStudy/src/main/java/com/yf/java/study/array/ArrayTest.java
@@ -0,0 +1,23 @@
+package com.yf.java.study.array;
+
+/**
+ * @Author: YangFei
+ * @Description: 数组Array测试
+ * @create: 2019-11-25 23:46
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        //声明数组1,声明一个长度为3，只能存放int类型的数据
+        int[] myArray1 = new int[3];
+        //声明数组2,声明一个数组元素为 1,2,3的int类型数组
+        int[] myArray2 = {1, 2, 3};
+        //给myArray第一个元素赋值1
+        myArray1[0] = 6;
+        //访问myArray的第一个元素
+        System.out.println(myArray1[1]);
+        for (int i = 0; i < myArray2.length; i++) {
+            System.out.println(myArray2[i]);
+
+        }
+    }
+}
