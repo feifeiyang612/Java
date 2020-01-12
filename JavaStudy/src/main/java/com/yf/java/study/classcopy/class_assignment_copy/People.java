2d34513f7c39d379de17be5955001e584024f6c7
yangfei <644752026@qq.com>
2020-01-12 23:44:10 +0800

    11

--- JavaStudy/src/main/java/com/yf/java/study/classcopy/class_assignment_copy/People.java
+++ JavaStudy/src/main/java/com/yf/java/study/classcopy/class_assignment_copy/People.java
@@ -2,7 +2,7 @@ package com.yf.java.study.classcopy.class_assignment_copy;
 
 /**
  * @Author: YangFei
- * @Description: 对象赋值的实例，对象赋值实质上是两个不同的对象指向内存堆中同一个对象
+ * @Description: 对象赋值的实例。对象赋值实质上是两个不同的对象指向内存堆中同一个对象
  * @create: 2019-12-10 22:12
  */
 public class People {
